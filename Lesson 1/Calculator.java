74f50ea2ff02f520d6efa420b61bdae7a6eb42cb
nikolay4294 <boss1811@yandex.ru>
2020-11-24 08:23:01 +0300

    исправил домашнее задание, перенес файл README.MD

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,34 +1,25 @@
 public class Calculator {
     public static void main(String[] args) {
-        int introducedNumberOne = 10;
-        int introducedNumberTwo = 15;
-        String mathematicalOperation = "*";
-        if(mathematicalOperation == "+") {
-            System.out.println(introducedNumberOne + introducedNumberTwo);
-        }
+        int a = 10;
+        int b = 15;
+        String mathOperation = "*";
 
-        else if(mathematicalOperation == "-") {
-            System.out.println(introducedNumberOne - introducedNumberTwo);
-        }
-
-        else if(mathematicalOperation == "*") {
-            System.out.println(introducedNumberOne * introducedNumberTwo);
-        }
-
-        else if(mathematicalOperation == "/") {
-            System.out.println(introducedNumberOne / introducedNumberTwo);
-        }
-
-        else if(mathematicalOperation == "^") {
+        if(mathOperation == "+") {
+            System.out.println(a + b);
+        } else if(mathOperation == "-") {
+            System.out.println(a - b);
+        } else if(mathOperation == "*") {
+            System.out.println(a * b);
+        } else if(mathOperation == "/") {
+            System.out.println(a / b);
+        } else if(mathOperation == "^") {
             long result = 1;
-            for(int i = 1; i <= introducedNumberTwo; i++) {
-                result = result * introducedNumberOne;
+            for(int i = 1; i <= b; i++) {
+                result *= a;
             }
             System.out.println(result);
-        }
-
-        else if(mathematicalOperation == "%") {
-            System.out.println(introducedNumberOne % introducedNumberTwo);
+        } else if(mathOperation == "%") {
+            System.out.println(a % b);
         } else {
               System.out.println("Такого математического действия не существует. Введите корректный символ");
         }
