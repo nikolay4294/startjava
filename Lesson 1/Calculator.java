f2d29f4f7e1406e3d8d5e36eab585fbc3d577e16
nikolay4294 <boss1811@yandex.ru>
2020-11-25 23:09:45 +0300

    Юникод. Поправил калькулятор

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -2,23 +2,23 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 10;
         int b = 15;
-        String mathOperation = "*";
+        char mathOperation = '*';
 
-        if(mathOperation == "+") {
+        if(mathOperation == '+') {
             System.out.println(a + b);
-        } else if(mathOperation == "-") {
+        } else if(mathOperation == '-') {
             System.out.println(a - b);
-        } else if(mathOperation == "*") {
+        } else if(mathOperation == '*') {
             System.out.println(a * b);
-        } else if(mathOperation == "/") {
+        } else if(mathOperation == '/') {
             System.out.println(a / b);
-        } else if(mathOperation == "^") {
+        } else if(mathOperation == '^') {
             long result = 1;
             for(int i = 1; i <= b; i++) {
                 result *= a;
             }
             System.out.println(result);
-        } else if(mathOperation == "%") {
+        } else if(mathOperation == '%') {
             System.out.println(a % b);
         } else {
               System.out.println("Такого математического действия не существует. Введите корректный символ");
--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -1,17 +1,19 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int a = 15; //загаданное компьютером число
-        int b = 0; //введеное пользователем число
+        int numComputer = 25;
+        int numPlayer = 20;
 
-        for(int i = 0; i < 100; i++) {
-            b = i;
-            if(a == b) {
-                 System.out.println("Вы угадали!");
-            } else if(a > b) {
-                System.out.println("Введенное вами число меньше того, что загадал компьютер");
-            } else if(a < b) {
-                System.out.println("Введенное вами число больше того, что загадал компьютер");
+            while(numPlayer <= 100) {
+                if(numComputer == numPlayer) {
+                    System.out.println("Вы угадали!");
+                    return;
+                } else if(numComputer > numPlayer) {
+                    System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                    numPlayer++;
+                } else if(numComputer < numPlayer) {
+                    System.out.println("Введенное вами число больше того, что загадал компьютер");
+                    numPlayer++;
+                }
             }
-        }
     }
 }
