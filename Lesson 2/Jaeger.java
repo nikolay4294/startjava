1680225fa22c874c12829db071847963eae6fa18
Nikolay4294 <boss1811@yandex.ru>
2020-12-09 23:37:57 +0300

    добавил класс Jaeger

---
+++ Lesson 2/Jaeger.java
@@ -0,0 +1,27 @@
+public class Jaeger{
+
+    String modelName = "Bracer Phoenix";
+    String mark = "Mark-5";
+    String origin = "USA";
+    Float height = 70.7;
+    Float weight = 2.1;
+    int speed = 3;
+    int strenght = 8;
+    int armor =9;
+
+    boolean drift() {
+
+    }
+
+    void move() {
+
+    }
+
+    String scanKaiju() {
+
+    }
+
+    void useVortexCannon() {
+
+    }
+}
