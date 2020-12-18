b9e005a7a79c6eb26c2ca229917b4359f9abd4dd
Nikolay4294 <boss1811@yandex.ru>
2020-12-18 11:09:04 +0300

    поправил код

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -1,7 +1,11 @@
 public class GuessNumber {
-    public GuessNumber(Player player1, Player player2){
+    Player player1;
+    Player player2;
 
-     }
+    public GuessNumber(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 
     public static int randomComputerNum() {
         Random random = new Random();
